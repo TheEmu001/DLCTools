e554579bc6481073ab25e956b5bc5dcb278e9de8
TheEmu001 <imehndiokho@gmail.com>
2020-05-14 13:59:34 -0500

    attempting to fix speed problem, still failing

--- VelocityUsingPandas.py
+++ VelocityUsingPandas.py
@@ -18,42 +18,52 @@ data_df = pd.read_csv(path, skiprows=3, names=['frameNo', 'snoutX', 'snoutY', 's
                                                 'lefthindpawlikelihood', 'righthindpawx', 'righthindpawy',
                                                 'righthindpawlikelihood', 'tailbasex', 'tailbasey', 'taillikelihood'])
 
-data_df['Time Elapsed'] = data_df['frameNo'] / 30
 
-xy = data_df[['snoutX', 'snoutY']]
+# parsing snout likelihood higher than 60%
+dataParsed = pd.DataFrame(data = data_df[data_df.snoutLike >= 0.6])
 
-b = np.roll(xy, -1, axis=0)[1:-1]
 
-a = xy[1:-1]
+dataParsed['Time'] = dataParsed.index/30
+dist = dataParsed.diff().fillna(0.)
 
-# change in xy
-dxy = np.linalg.norm(a - b, axis=1)
 
-# change in time
-dt = (np.roll(data_df['Time Elapsed'], -1) - data_df['Time Elapsed'])[1:-1]
+dist['Dist'] = np.sqrt(dist.snoutX**2 + dist.snoutY**2)
+dist['speed'] = dist.Dist/(dist.Time)
 
-# calculating the speed, change in displacement over time
-speeds = np.divide(dxy, dt)
-
-speed_df = pd.DataFrame(data={'Time Elapsed': data_df['Time Elapsed'][1:-1], 'Speed': speeds, 'Snout Likelihood': data_df['snoutLike'][1:-1]})
-
-speed_normalized = (speed_df - speed_df.mean())/speed_df.std()
-
-# cummulative moving average
-# data_df['CMA'] = speed_df['Speed'][1:-1].expanding(min_periods=10).mean()
-speed_df['CMA'] = speed_df['Speed'][1:-1].expanding(min_periods=2).mean()
-
-# speed_df['EMA'] = speed_normalized['Speed'].ewm(span=40,adjust=False).mean()
-# for i in range(0,speed_df.shape[0]-2):
-#     speed_df.loc[speed_df.index[i+2],'SMA_3'] = np.round(((speed_df.iloc[i,1]+ speed_df.iloc[i+1,1] +speed_df.iloc[i+2,1])/3),1)
-# print(speed_df.head())
-
-speed_df['pandas_SMA_3'] = speed_df.iloc[:,1].rolling(window=2).mean()
-
-plt.plot(speed_df['Time Elapsed'], speed_df['Speed'],color='blue', marker='o', markersize=0.1, linewidth=0.1, label='Raw Data')
-plt.plot(speed_df['Time Elapsed'], speed_df['pandas_SMA_3'],color='red', marker='o', markersize=0.1, linewidth=0.5, label='pandas_SMA_3')
-plt.plot(speed_df['Time Elapsed'], speed_df['CMA'],color='green', marker='o', markersize=0.1, linewidth=0.5, label='CMA')
-
+print(dist)
+# xy = data_df[['snoutX', 'snoutY']]
+# b = np.roll(xy, -1, axis=0)[1:-1]
+#
+# a = xy[1:-1]
+#
+# # change in xy
+# dxy = np.linalg.norm(a - b, axis=1)
+#
+# # change in time
+# dt = (np.roll(data_df['Time Elapsed'], -1) - data_df['Time Elapsed'])[1:-1]
+#
+# # calculating the speed, change in displacement over time
+# speeds = np.divide(dxy, dt)
+#
+# speed_df = pd.DataFrame(data={'Time Elapsed': data_df['Time Elapsed'][1:-1], 'Speed': speeds, 'Snout Likelihood': data_df['snoutLike'][1:-1]})
+#
+# speed_normalized = (speed_df - speed_df.mean())/speed_df.std()
+#
+# # cummulative moving average
+# # data_df['CMA'] = speed_df['Speed'][1:-1].expanding(min_periods=10).mean()
+# speed_df['CMA'] = speed_df['Speed'][1:-1].expanding(min_periods=2).mean()
+#
+# # speed_df['EMA'] = speed_normalized['Speed'].ewm(span=40,adjust=False).mean()
+# # for i in range(0,speed_df.shape[0]-2):
+# #     speed_df.loc[speed_df.index[i+2],'SMA_3'] = np.round(((speed_df.iloc[i,1]+ speed_df.iloc[i+1,1] +speed_df.iloc[i+2,1])/3),1)
+# # print(speed_df.head())
+#
+# speed_df['pandas_SMA_3'] = speed_df.iloc[:,1].rolling(window=2).mean()
+#
+# plt.plot(speed_df['Time Elapsed'], speed_df['Speed'],color='blue', marker='o', markersize=0.1, linewidth=0.1, label='Raw Data')
+# plt.plot(speed_df['Time Elapsed'], speed_df['pandas_SMA_3'],color='red', marker='o', markersize=0.1, linewidth=0.5, label='pandas_SMA_3')
+plt.plot(dist['Time'], dist['speed'],color='green', marker='o', markersize=0.1, linewidth=0.5, label='CMA')
+#
 plt.xlabel('time (seconds)')
 plt.ylabel('velocity (Pixels/second)')
 plt.legend(loc=2)
